1a94db9949d8a5da1edb9626079af1406028f1e1
Jgp19112003 <jgp19112003@gmail.com>
2021-11-11 11:28:23 +0100

    update

---
+++ T4/T4_Arrays/src/Arrays.java
@@ -0,0 +1,27 @@
+public class Arrays {
+    public static void main(String[] args) {
+        //array numeros
+        int[] numeros = new int[10];
+        int [] numerosDos = {1,2,3,4,5,6,7,8,9,10};
+       boolean[] aciertos = new boolean[5];
+        String[] palabras = new String[3];
+        double[] decimales = new double[2];
+
+        int longuitudArray = numerosDos.length;
+        System.out.println(numerosDos [0]);
+        System.out.println(numerosDos [1]);
+        System.out.println(numerosDos [2]);
+        System.out.println(numerosDos [3]);
+        System.out.println(numerosDos [4]);
+        System.out.println(numerosDos [5]);
+        System.out.println(numerosDos [6]);
+        System.out.println(numerosDos [7]);
+        System.out.println(numerosDos [8]);
+        System.out.println(numerosDos [9]);
+
+
+
+
+
+    }
+}
