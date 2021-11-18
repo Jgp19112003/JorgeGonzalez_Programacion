fcd45531e84bede7cd0b5cd3e0d71cf7e3ba6a2c
Jgp19112003 <jgp19112003@gmail.com>
2021-11-18 11:11:30 +0100

    update

---
+++ T4/T4_Arrays/src/Ejercicios_Arrays_md/Ej6.java
@@ -0,0 +1,36 @@
+package Ejercicios_Arrays_md;
+import java.util.Scanner;
+
+public class Ej6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int longuitud,aux;
+
+        System.out.println("De que longuitud desea que sea su array: ");
+        longuitud = in.nextInt();
+        int[] nums = new int[longuitud];
+
+
+        for (int i = 0; i < nums.length; i++) {
+
+            System.out.printf("Introduzca el numero %d:%n", i + 1);
+            nums[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < nums.length-1; i++) {
+                aux = nums[i+1];
+                nums[i+1] = nums[0];
+                nums[0] = aux;
+            }
+
+        for (int item: nums) {
+            System.out.println(item);
+
+        }
+
+
+        }
+
+
+   }
+
