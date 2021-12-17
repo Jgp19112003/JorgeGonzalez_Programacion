2cc5ae38fef243b16371bdf8c1eb93b0c6426f24
Jgp19112003 <jgp19112003@gmail.com>
2021-12-17 12:42:22 +0100

    update

---
+++ T4/T4_Arrays/src/HasTable/ListasHash.java
@@ -0,0 +1,26 @@
+package HasTable;
+
+import java.util.Hashtable;
+
+public class ListasHash {
+    public static void main(String[] args) {
+
+        Hashtable listaElementos = new Hashtable();
+
+        listaElementos.size();
+        listaElementos.put("1","Elemento1");
+        listaElementos.put("2","Elemento2");
+        listaElementos.put("3",false);
+        listaElementos.put("4",new String[]{"Cosa1","Cosa2","Cosa3"});
+
+        System.out.println(listaElementos.size());
+
+        String[] elementoEncontrado = (String[]) listaElementos.get("4");
+        elementoEncontrado[0] = "Nuevo valor";
+        listaElementos.remove("1");
+
+        for (String item: elementoEncontrado) {
+            System.out.println(item);
+        }
+    }
+}
